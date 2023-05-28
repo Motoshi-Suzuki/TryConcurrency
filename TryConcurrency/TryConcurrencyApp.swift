81d4d3a6990635f1511d4d83bac0b91829f065a9
motoshi-suzuki <motoshi.suzukiii@gmail.com>
2023-05-28 10:06:05 +0900

    initial commit

---
+++ TryConcurrency/TryConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryConcurrencyApp.swift
+//  TryConcurrency
+//
+//  Created by Motoshi Suzuki on 2023/05/28.
+//
+
+import SwiftUI
+
+@main
+struct TryConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
