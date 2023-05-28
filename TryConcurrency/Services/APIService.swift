af0853d144f1c575975636f2e80893beb717ec47
motoshi-suzuki <motoshi.suzukiii@gmail.com>
2023-05-28 11:50:42 +0900

    use result type

--- TryConcurrency/Services/APIService.swift
+++ TryConcurrency/Services/APIService.swift
@@ -10,23 +10,42 @@ import Foundation
 struct APIService {
     let urlString: String
 
-    func getUsers(completion: @escaping ([User]) -> Void) {
-        guard let url = URL(string: urlString) else { return }
-
+    func getUsers(completion: @escaping (Result<[User], APIError >) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidURL))
+            return
+        }
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let httpRes = response as? HTTPURLResponse,
-                  httpRes.statusCode == 200 else { return }
-            guard error == nil else { return }
-            guard let data else { return }
+                  httpRes.statusCode == 200 else {
+                completion(.failure(.invalidResponseStatus))
+                return
+            }
+            guard error == nil else {
+                completion(.failure(.dataTaskError))
+                return
+            }
+            guard let data else {
+                completion(.failure(.corruptData))
+                return
+            }
 
             let decoder = JSONDecoder()
             do {
                 let decodedData = try decoder.decode([User].self, from: data)
-                completion(decodedData)
+                completion(.success(decodedData))
             } catch {
-                print("Unnn error")
+                completion(.failure(.decodingError))
             }
         }
         .resume()
     }
+}
+
+enum APIError: Error {
+    case invalidURL
+    case invalidResponseStatus
+    case dataTaskError
+    case corruptData
+    case decodingError
 }
